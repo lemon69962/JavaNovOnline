2fe1aaaeb463e6b5fca4f07a00675cfd94fbc8b3
Lernard Kong <148523297+lemon69962@users.noreply.github.com>
2024-11-26 16:38:09 +0800

    update my code

---
+++ Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    public static void main(String[] args) {
+        String name = "Kong Xien Voon";
+        int age = 24;
+        double height = 1.64;
+        boolean isStudent = true;
+        long netWorth = 250000000L;
+
+        System.out.println("My name is  " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("I am " + height + "m tall");
+        System.out.println("I am a student: " + isStudent);
+        System.out.println("My net worth is RM" + netWorth);
+
+
+    }
+}
