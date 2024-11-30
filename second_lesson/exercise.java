df60a9b9c891323f60c69b98154361be5e9ed96e
Lernard Kong <148523297+lemon69962@users.noreply.github.com>
2024-11-30 12:46:19 +0800

    update exercise3

---
+++ second_lesson/exercise.java
@@ -0,0 +1,14 @@
+package second_lesson;
+
+public class exercise{
+
+    public static void main(String[] args) {
+        for(int i=1 ;i<=26 ;i++){
+            if(i!=14)
+            System.out.println(i);
+        }
+    }
+
+
+
+}
